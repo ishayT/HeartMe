3f3c55357c3a367d02b3f982e768318ded168032
Ishay <ishay@ishays-mac.local>
2018-08-01 02:40:34 +0300

    Initial Commit

---
+++ HeartMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeartMe
+//
+//  Created by Ishay on 8/1/18.
+//  Copyright © 2018 Ishay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeartMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeartMe
+//
+//  Created by Ishay on 8/1/18.
+//  Copyright © 2018 Ishay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
